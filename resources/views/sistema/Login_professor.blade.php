ac757db7134ecabb50da50a498b66abfa95067da
Giovanny Lima <didiosilva@gmail.com>
2019-10-17 20:56:30 -0400

    Funcionando login de aluno e professor  e funcionando cadastro aluno e professor

--- resources/views/sistema/Login_professor.blade.php
+++ resources/views/sistema/Login_professor.blade.php
@@ -1,116 +1,154 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-	
+
 	<meta charset="utf-8">
 
-	
-	
+
+
 	<link href="libs/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Lobster&display=swap" rel="stylesheet">
 
 
-    <style>
-    	body{
-	margin: 0;
-	background-image: url(imagens/azul.jpg);
-	width: 100%;
-	height: 100%;
-}
-.centertext{
-	margin: 0 auto 0 auto;
-}
-#fonte{
-	font-family: FreeMono, monospace;
-	font-size: 20px;
-	color: white; 
-	text-align: center;
-}
-#fonte2{
-	font-family: FreeMono, monospace;
-	font-size: 20px;
-	color: white; 
-	margin-left: 15px;
-}
-.nav{
-	background-color:;
-	width:100%;
-	height: 120px;
-}
-.divform{
-	align-items: center;
-	background-image: url(imagens/azul2.jpg); 
-	width:400px;
-	height: 300px;
-	margin: 0 auto 0 auto;
-	border-radius: 25px;
+	<style>
+		body {
+			margin: 0;
+			background-image: url(imagens/azul.jpg);
+			width: 100%;
+			height: 100%;
+		}
+		.centertext {
+			margin: 0 auto 0 auto;
+		}
+		#fonte {
+			font-family: FreeMono, monospace;
+			font-size: 20px;
+			color: white;
+			text-align: center;
+		}
+		#fonte2 {
+			font-family: FreeMono, monospace;
+			font-size: 20px;
+			color: white;
+			margin-left: 15px;
+		}
+		.nav {
+			background-color: ;
+			width: 100%;
+			height: 120px;
+		}
+		.divform {
+			align-items: center;
+			background-image: url(imagens/azul2.jpg);
+			width: 400px;
+			height: 300px;
+			margin: 0 auto 0 auto;
+			border-radius: 25px;
+		}
+		.divuni {
+			margin: 0 auto 0 auto;
+		}
+		.Ctext {
+			margin-left: 15px;
+			margin-right: 15px;
+		}
+		div.alert.alert-succes {
+			text-align: center;
+		}
+		.central {
+			text-align: center;
+		}
+	</style>
 
-}
-.divuni{
-	margin: 0 auto 0 auto;
-}
-.Ctext{
-	margin-left:15px;
-	margin-right: 15px; 
-}
-    </style>
 	<title>Login Professor</title>
 
 </head>
+
 <body>
 
-@if(empty($matricula))
-  
-  <div  class="alert alert-success">
-                           Bem Vindo
-  </div>
-  
-@else
-  <div class="alert alert-success">
-    Sua matricula para acesso ao sistema é :  {{$matricula}}.
-  </div>
-@endif
+	@if(empty($matricula))
+
+	<div id="central" class="alert alert-success central">
+		Bem Vindo
+	</div>
+
+	@else
+	<div id="central1" class="alert alert-success central">
+		Sua matricula para acesso ao sistema é : {{$matricula}}.
+	</div>
+	@endif
 
 	<div class="conteiner-fluid">
-		
+
 		<div class="nav">
 			<div class="divuni"><img src="imagens/Uninorte.jpg"></div>
 		</div>
-			<br>
-			<div class="divform"><br>
-				<form>
-	  				<div class="form-group">
-	  					<p id="fonte">Professor</p>
-	  					
-	  					<br>
-	    				<div class="Ctext">
-	    				<input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Login">
-	    				<small id="emailHelp" class="form-text text-muted"></small>
-	    				</div>
+		<br>
 
-	 				</div>
-					<div class="form-group">
-					
-	    				<div class="Ctext">
-	    				<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha">
-	    				</div>
+		<div class="divform"><br>
 
-	  				</div>
-	  				<div id="fonte">
+			<form method="POST" action="{{ route('login') }}">
+				{{ csrf_field() }}
 
-					  <a href="{{route('Cadastro_aluno')}}" class="btn btn-primary">
-					  Login
-				    </a>
-					
+				<div class="form-group">
+					<p id="fonte">Professor</p>
+
+					<br>
+
+                    <div class="Ctext">
+                                <input id="email" type="email" placeholder="Email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                    </div>
+
+					<!--<div class="Ctext">
+						<input type="text" class="form-control" id="InputEmail" aria-describedby="emailHelp" placeholder="Login">
+						
+					</div>-->
+
+				</div>
+
+				<div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <!--<label for="password" class="col-md-4 control-label">Password</label>-->
+
+                            <div class="Ctext">
+                                <input id="password" type="password" class="form-control" name="password" placeholder="Senha" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                </div>
+
+				<!--<div class="form-group">
+					<div class="Ctext">
+						<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha">
+					</div>
+				</div>-->
+
+				<div id="fonte">
+
+				      <button type="submit" class="btn btn-primary">
+                                    Login
+                      </button>
+
 
 					<a href="{{route('Cadastro_professor')}}" class="btn btn-primary">
 						Cadastre-se
-				    </a>
-					</div>
-				</form>
-			</div>
+					</a>
+				</div>
+			</form>
+		</div>
 	</div>
 
 
 </body>
+
 </html>
--- storage/framework/views/d92515bb7f3ea6d16096205a442ca05b68c31fd9.php
+++ storage/framework/views/d92515bb7f3ea6d16096205a442ca05b68c31fd9.php
@@ -1,127 +1,138 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-	
+
 	<meta charset="utf-8">
 
-	
+
 	<link href="libs/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Lobster&display=swap" rel="stylesheet">
-	
+
 	<title>Login Aluno</title>
 
 	<style>
-body{
-	margin: 0;
-	background-image: url(imagens/azul.jpg);
-	width: 100%;
-	height: 100%;
-}
-.centertext{
-	margin: 0 auto 0 auto;
-}
-#fonte{
-	font-family: FreeMono, monospace;
-	font-size: 20px;
-	color: white; 
-	text-align: center;
-}
-#fonte2{
-	font-family: FreeMono, monospace;
-	font-size: 20px;
-	color: white; 
-	margin-left: 15px;
-}
-.nav{
-	background-color:;
-	width:100%;
-	height: 120px;
-}
-.divform{
-	align-items: center;
-	background-image: url(imagens/azul2.jpg); 
-	width:400px;
-	height: 300px;
-	margin: 0 auto 0 auto;
-	border-radius: 25px;
+		body {
+			margin: 0;
+			background-image: url(imagens/azul.jpg);
+			width: 100%;
+			height: 100%;
+		}
 
-}
-.divuni{
-	margin: 0 auto 0 auto;
-}
-.Ctext{
-	margin-left:15px;
-	margin-right: 15px; 
-}
-#btn1{
-	 
-}
+		.centertext {
+			margin: 0 auto 0 auto;
+		}
+
+		#fonte {
+			font-family: FreeMono, monospace;
+			font-size: 20px;
+			color: white;
+			text-align: center;
+		}
+
+		#fonte2 {
+			font-family: FreeMono, monospace;
+			font-size: 20px;
+			color: white;
+			margin-left: 15px;
+		}
+
+		.nav {
+			background-color: ;
+			width: 100%;
+			height: 120px;
+		}
+
+		.divform {
+			align-items: center;
+			background-image: url(imagens/azul2.jpg);
+			width: 400px;
+			height: 300px;
+			margin: 0 auto 0 auto;
+			border-radius: 25px;
+
+		}
+
+		.divuni {
+			margin: 0 auto 0 auto;
+		}
+
+		.Ctext {
+			margin-left: 15px;
+			margin-right: 15px;
+		}
+
+		#btn1 {}
+			.alert{
+			text-align: center;
+		}
 	</style>
 
 </head>
+
 <body>
 
-<?php if(empty($matricula)): ?>
-  <div class="alert alert-success">
-                           Bem Vindo
-  </div>
-  
-<?php else: ?>
-  <div class="alert alert-success">
-    Sua matricula para acesso ao sistema é :  <?php echo e($matricula); ?>.
-  </div>
-<?php endif; ?>
+	<?php if(empty($matricula)): ?>
+	<div class="alert alert-success">
+		Bem Vindo
+	</div>
+
+	<?php else: ?>
+	<div class="alert alert-success">
+		Sua matricula para acesso ao sistema é : <?php echo e($matricula); ?>.
+	</div>
+	<?php endif; ?>
 
 
 	<div class="conteiner-fluid">
-		
+
 		<div class="nav">
 			<div class="divuni"><img src="imagens/Uninorte.jpg"></div>
 		</div>
-			<br>
-			<div class="divform"><br>
-				<form method="POST" action="<?php echo e(route('login')); ?>">
-                        <?php echo e(csrf_field()); ?>
+		<br>
+		<div class="divform"><br>
+			<form method="POST" action="<?php echo e(route('login')); ?>">
+				<?php echo e(csrf_field()); ?>
 
 
 
-	  				<div class="form-group">
-	  					<p id="fonte">Aluno</p>
-  						<br>
-                        
-						<div class="Ctext">
-                                <input id="email" type="email" placeholder="Email" class="form-control" name="email" value="<?php echo e(old('email')); ?>" required autofocus>
+				<div class="form-group">
+					<p id="fonte">Aluno</p>
+					<br>
 
-                                <?php if($errors->has('email')): ?>
-                                    <span class="help-block">
-                                        <strong><?php echo e($errors->first('email')); ?></strong>
-                                    </span>
-                                <?php endif; ?>
-                        </div>
+					<div class="Ctext">
+						<input id="email" type="email" placeholder="Email" class="form-control" name="email" value="<?php echo e(old('email')); ?>" required autofocus>
 
-	    				<!--<div class="Ctext">
+						<?php if($errors->has('email')): ?>
+						<span class="help-block">
+							<strong><?php echo e($errors->first('email')); ?></strong>
+						</span>
+						<?php endif; ?>
+					</div>
+
+					<!--<div class="Ctext">
 	    				<input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Matricula">
 	    				<small id="emailHelp" class="form-text text-muted"></small>
 	    				</div>-->
 
 
-	 				</div>
-                     
-					 <div class="form-group<?php echo e($errors->has('password') ? ' has-error' : ''); ?>">
-                            <!--<label for="password" class="col-md-4 control-label">Password</label>-->
+				</div>
 
-                            <div class="Ctext">
-                                <input id="password" type="password" class="form-control" name="password" placeholder="Senha" required>
+				<div class="form-group<?php echo e($errors->has('password') ? ' has-error' : ''); ?>">
+					<!--<label for="password" class="col-md-4 control-label">Password</label>-->
 
-                                <?php if($errors->has('password')): ?>
-                                    <span class="help-block">
-                                        <strong><?php echo e($errors->first('password')); ?></strong>
-                                    </span>
-                                <?php endif; ?>
-                            </div>
-                    </div>
+					<div class="Ctext">
+						<input id="password" type="password" class="form-control" name="password" placeholder="Senha" required>
 
-					<!--<div class="form-group">
+						<?php if($errors->has('password')): ?>
+						<span class="help-block">
+							<strong><?php echo e($errors->first('password')); ?></strong>
+						</span>
+						<?php endif; ?>
+					</div>
+				</div>
+
+				<!--<div class="form-group">
 						
 						
 	    				<div class="Ctext">
@@ -132,20 +143,21 @@ body{
 
 	  				</div>-->
 
-	  				<div id="fonte">
+				<div id="fonte">
 
-					  <button type="submit" class="btn btn-primary">
-                                    Login
-                      </button>
-                    
-					   <a href="<?php echo e(route('Cadastro_aluno')); ?>" class="btn btn-primary">
+					<button type="submit" class="btn btn-primary">
+						Login
+					</button>
+
+					<a href="<?php echo e(route('Cadastro_aluno')); ?>" class="btn btn-primary">
 						Cadastre-se
-				       </a>
-				    </div>
+					</a>
+				</div>
 			</form>
 		</div>
 	</div>
 
 
 </body>
+
 </html>
