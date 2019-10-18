77cbde17fabd829f1d9e6539c08b3fd90c3532fa
Giovanny Lima <didiosilva@gmail.com>
2019-10-17 20:22:34 -0400

    realizando login de aluno e professor e cadastro

--- resources/views/sistema/Index.blade.php
+++ resources/views/sistema/Index.blade.php
@@ -1,12 +1,13 @@
 <!doctype html>
 <!--<html lang="pt-br">-->
-<!doctype html><html lang="pt-br">
+<!doctype html>
+<html lang="pt-br">
 
 <head>
-	
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<link rel="stylesheet" type="text/css" href="inde.css">
 
@@ -14,89 +15,107 @@
 	<link href="libs/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	<style>
-      body{
-	margin: 0;
-	background-image: url(imagens/azul.jpg);
-	width: 100%;
-	height: 100%;
-}
-.centertext{
-	margin: 0 auto 0 auto;
-}
-#fonte{
-	font-family: FreeMono, monospace;
-	font-size: 30px;
-	color: white; 
-	text-align: center;
-}
-#fonte2{
-	font-family: FreeMono, monospace;
-	font-size: 20px;
-	color: white; 
-	margin-left: 15px;
-}
-.nav{
-	width:100%;
-	height: 120px;
-}
-.divform{
-	align-items: center;
-	background-image: url(imagens/azul2.jpg); 
-	width:400px;
-	height: 300px;
-	margin: 0 auto 0 auto;
-	border-radius: 25px;
+		body {
+			margin: 0;
+			background-image: url(imagens/azul.jpg);
+			width: 100%;
+			height: 100%;
+		}
 
-}
-.divuni{
-	margin: 0 auto 0 auto;
-}
-.Ctext{
-	margin-left:15px;
-	margin-right: 15px; 
-}
+		.centertext {
+			margin: 0 auto 0 auto;
+		}
 
+		#fonte {
+			font-family: FreeMono, monospace;
+			font-size: 30px;
+			color: white;
+			text-align: center;
+		}
+
+		#fonte2 {
+			font-family: FreeMono, monospace;
+			font-size: 20px;
+			color: white;
+			margin-left: 15px;
+		}
+
+		.nav {
+			width: 100%;
+			height: 120px;
+		}
+
+		.divform {
+			align-items: center;
+			background-image: url(imagens/azul2.jpg);
+			width: 400px;
+			height: 300px;
+			margin: 0 auto 0 auto;
+			border-radius: 25px;
+
+		}
+
+		.divuni {
+			margin: 0 auto 0 auto;
+		}
+
+		.Ctext {
+			margin-left: 15px;
+			margin-right: 15px;
+		}
 	</style>
-	
+
 	<title>UniProjetos</title>
 
 </head>
+
 <body>
 
 	<div class="conteiner-fluid">
-		
+
 		<div class="nav">
 			<div class="divuni"><img src="imagens/Uninorte.jpg"></div>
 		</div>
+		<br>
+		<div class="divform"><br><br>
+
 			<br>
-			<div class="divform"><br><br>
-				
-	  				<br>
 
-	  				<div id="fonte">
-	  				<!--<a href="Login_aluno.html">-->
-					 <a href="{{route('Login_aluno')}}">
-						<button type="submit"  class="btn btn-primary" id="fonte2">Aluno</button>
-					</a>
-					</div><br><br>
+			<div id="fonte">
+				<!--<a href="Login_aluno.html">-->
+				<a href="{{route('Login_aluno')}}">
+					<button type="submit" class="btn btn-primary" id="fonte2">Aluno</button>
+				</a>
+			</div><br><br>
 
-					<div id="fonte">
-					
-					
+			<div id="fonte">
 
-					<a href="{{route('Login_professor')}}">
-						<button  type="submit" class="btn btn-primary" id="fonte2">Professor</button>
-					</a>
-					</div>
-				
+
+
+				<a href="{{route('Login_professor')}}">
+					<button type="submit" class="btn btn-primary" id="fonte2">Professor</button>
+				</a>
 			</div>
+			
+			@if ($errors->any())
+			<div class="alert alert-danger">
+				<ul>
+					@foreach ($errors->all() as $error)
+					<li>{{ $error }}</li>
+					@endforeach
+				</ul>
+			</div>
+			@endif
+
+		</div>
 	</div>
 
 
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,11 @@
 
 
 Route::get('/', function () {
-    return view('sistema.Index');
+  return view('sistema.Index');
+    //return view('welcome');
 });
+
+
 
 
 Route::get('/home', 'HomeController@index')->name('home');
@@ -13,7 +16,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::post('/create','testeController@store')->name('store');
 
-Route::post('/store','alunController@store')->name('store.aluno');
+//ute::post('/store','alunController@store')->name('store.aluno');
 
 Route::post('/store/professor','ProfessorController@store')->name('store.professor');
 
