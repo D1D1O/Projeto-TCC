e3730675357ec16f86b2b5b66509be924a4e1d27
Giovanny Lima <didiosilva395@gmail.com>
2019-11-05 19:07:52 -0400

    adicionado coluna matricula a aqual sera atribuido o ai do usuario

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,6 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('matricula')->unique();
             $table->string('name');
             $table->string('avatar')->nullable();
             $table->string('tipo');
